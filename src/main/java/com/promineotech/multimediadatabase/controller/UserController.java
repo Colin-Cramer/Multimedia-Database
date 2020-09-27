df305a9e9103520cb0e5da8efb4a2f6222857c01
Colin Cramer <colin.w.cramer@gmail.com>
2020-09-27 09:12:41 -0700

    User Controller, Service

--- src/main/java/com/promineotech/multimediadatabase/controller/UserController.java
+++ src/main/java/com/promineotech/multimediadatabase/controller/UserController.java
@@ -1,10 +1,54 @@
 package com.promineotech.multimediadatabase.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.promineotech.multimediadatabase.entity.User;
+import com.promineotech.multimediadatabase.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+	@Autowired
+	private UserService service;
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public ResponseEntity<Object> register(@RequestBody User user) {
+		return new ResponseEntity<Object>(service.createUser(user), HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<Object> login(@RequestBody User user) {
+		try {
+			return new ResponseEntity<Object>(service.login(user), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Object> updateUser(@RequestBody User user, @PathVariable Long id) {
+		try {
+			return new ResponseEntity<Object>(service.updateUser(user, id), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Object> deleteUser(@PathVariable Long id) {
+		try {
+			service.deleteUser(id);
+			return new ResponseEntity<Object>("Successfully deleted user with id: " + id, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
 }
--- src/main/java/com/promineotech/multimediadatabase/service/UserService.java
+++ src/main/java/com/promineotech/multimediadatabase/service/UserService.java
@@ -1,5 +1,7 @@
 package com.promineotech.multimediadatabase.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,8 @@ import com.promineotech.multimediadatabase.repository.UserRepository;
 
 @Service
 public class UserService {
+	
+	private static final Logger logger = LogManager.getLogger(UserService.class);
 
 	@Autowired
 	private UserRepository repo;
@@ -22,6 +26,28 @@ public class UserService {
 			return foundUser;
 		} else {
 			throw new Exception("Invalid username or password, please try again.");
+		}
+	}
+	
+	public User updateUser(User user, Long id) throws Exception {
+		try {
+			User oldUser = repo.findOne(id);
+			oldUser.setUsername(user.getUsername());
+			oldUser.setPassword(user.getPassword());
+			oldUser.setEmail(user.getEmail());
+			return repo.save(oldUser);
+		} catch (Exception e) {
+			logger.error("Exception occurred while trying to update user: " + id, e);
+			throw new Exception ("Unable to update user.");
+		}
+	}
+	
+	public void deleteUser(Long id) throws Exception {
+		try {
+			repo.delete(id);
+		} catch (Exception e) {
+			logger.error("Exception occurred while trying to delete user: " + id, e);
+			throw new Exception("Unable to delete user.");
 		}
 	}
 	
